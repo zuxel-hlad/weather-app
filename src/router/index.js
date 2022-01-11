d504f8dd72f29bbb93d0021a8906be451320008c
Stanislav <stanislavschelokov@gmail.com>
2022-01-11 19:52:08 +0200

    clear App.vue

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [],
+  linkActiveClass: 'active',
+  linkExactPathActiveClass: 'active'
+})
+
+export default router
