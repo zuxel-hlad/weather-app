f87acfc37322e3527faff9c4d19d204ec997cc84
Stanislav <stanislavschelokov@gmail.com>
2022-01-20 21:51:53 +0200

    replaced text in weather card button component to svg icons

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -4,8 +4,18 @@
       <h2 class="card-title">{{ city.name }}</h2>
       <span class="card-location"></span>
       <div class="card-header-btns">
-        <button class="card-button" type="button" @click.stop="$emit('updateInfo')">Обновить</button>
-        <button class="card-button" type="button" @click.stop="$emit('delete')">Удалить</button>
+        <button class="card-button" title="обновить данные о погоде" type="button" @click.stop="$emit('updateInfo')">
+          <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path
+              d="M446.452,231.973A192.112,192.112,0,0,0,285.51,66.251l9.57-9.493a21.333,21.333,0,1,0-30.048-30.292L219.6,71.536a21.333,21.333,0,0,0-.123,30.169l45.067,45.44A21.333,21.333,0,0,0,294.834,117.1l-7-7.053A149.412,149.412,0,0,1,349.307,372.6a21.334,21.334,0,1,0,26.7,33.279A190.874,190.874,0,0,0,446.452,231.973Z"/>
+            <path
+              d="M247.421,364.863a21.333,21.333,0,0,0-30.294,30.046l7,7.055a149.413,149.413,0,0,1-61.435-262.585A21.334,21.334,0,1,0,136,106.091,192.091,192.091,0,0,0,226.438,445.77l-9.556,9.479a21.333,21.333,0,0,0,30.048,30.292l45.434-45.07a21.334,21.334,0,0,0,.123-30.169Z"/>
+          </svg>
+        </button>
+        <button class="card-button" title="удалить выбранный город" type="button" @click.stop="$emit('delete')">
+          <svg viewBox="0 0 512 512"
+               xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><path d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z"/></svg>
+        </button>
       </div>
     </div>
     <div class="card-weather">
@@ -75,11 +85,9 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
+
     &-btns {
       display: flex;
-      flex-direction: column;
-      align-items: flex-end;
-      justify-content: center;
     }
   }
 
@@ -89,17 +97,29 @@ export default {
   }
 
   &-button {
-    padding: 5px 10px;
-    min-width: 85px;
-    background-color: #fff;
-    border: 1px solid #ccc;
+    width: 25px;
+    height: 25px;
     border-radius: 4px;
     cursor: pointer;
     text-align: center;
     font-size: 14px;
+    padding: 0;
+    border: none;
+    background-color: transparent;
+    color: #ffffff;
+    margin: 0 0 0 10px;
 
-    &:active {
-      box-shadow: inset 1px 1px 1px rgba(0, 0, 0, 0.3);
+    svg {
+      width: 100%;
+      height: 100%;
+      fill: white;
+      transition: fill .4s ease;
+    }
+
+    &:hover {
+      svg {
+        fill: darkgreen;
+      }
     }
   }
 
